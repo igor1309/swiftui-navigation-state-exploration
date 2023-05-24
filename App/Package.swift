dffd3bd31d3e226fdd95098a8f2c48d99c5fdea9
Igor <igor1309@gmail.com>
2023-05-24 22:53:56 +0300

    add empty project

---
+++ App/Package.swift
@@ -0,0 +1,10 @@
+// swift-tools-version: 5.8
+// Empty Swift Package to hide app from Xcode.
+
+import PackageDescription
+
+let package = Package(
+    name: "",
+    products: [],
+    targets: []
+)
---
+++ App/SwiftUINavigationAsState/SwiftUINavigationAsState/SwiftUINavigationAsStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationAsStateApp.swift
+//  SwiftUINavigationAsState
+//
+//  Created by Igor Malyarov on 24.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationAsStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
